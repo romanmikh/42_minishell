2391f100fdab3fcae1c1cab30f8ad7d6c2d07e7c
¨Roman <roman.mikhaylenko14@hotmail.com>
2024-06-03 13:46:25 +0100

    Add: ft_strndup, **parse_input (<, >, <<, >>, ', ")

--- lib/libft/libft.h
+++ lib/libft/libft.h
@@ -48,6 +48,7 @@ char	*ft_strdup(const char *s);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	**ft_split(char const *s, char c);
+char	*ft_strndup(const char *s, size_t n);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
--- src/main.c
+++ src/main.c
@@ -24,6 +24,75 @@ void	handle_args(t_token **tokens, char **argv)
 	}
 }
 
+// to replace ft_split(' ') in main()
+char **parse_input(char *input)
+{
+  int pos;
+  char *token;
+  char **tokens;
+  char *delim;
+  int bufsize;
+
+  pos = 0;
+  bufsize = 64;
+  delim = " ";
+  tokens = malloc(bufsize * sizeof(char *));
+  if (!tokens)
+  {
+    ft_printf("@maxishell: allocation error\n");
+    exit(EXIT_FAILURE);
+  }
+  while (*input)
+  {
+    while (*input && ft_strchr(delim, *input))
+      input++;
+    if (*input == '\0')
+      break;
+    if (*input == '\'' || *input == '\"')
+    {
+      char quote;
+      char *start;
+
+      quote = *input++;
+      start = input;
+      while (*input && *input != quote)
+        input++;
+      if (*input == '\0')
+      {
+        ft_printf("@maxishell: syntax error: unmatched %c\n", quote);
+        free(tokens);
+        return (NULL);
+      }
+      *input = '\0'; 
+      token = ft_strdup(start);
+      input++;
+    }
+
+    else if (*input == '|' || *input == '<' || *input == '>')
+    {
+      tokens[pos++] = ft_strndup(input, 1);
+      if ((*input == '<' && *(input + 1) == '<') || (*input == '>' && *(input + 1) == '>'))
+      {
+        tokens[pos - 1] = ft_strndup(input, 2);
+        input++;
+      }
+      input++;
+    }
+    else
+    {
+      char *start;
+
+      start = input; 
+      while (*input && !ft_strchr(delim, *input) && *input != '|' && *input != '<' && *input != '>')
+        input++;
+      token = ft_strndup(start, input - start);
+      tokens[pos++] = token;
+    }
+  }
+  tokens[pos] = NULL;
+  return (tokens);
+}
+
 // getpwuid(geteuid())->pw_name // username, but uses disallowed functions
 int	main(void)
 {
@@ -46,14 +115,18 @@ int	main(void)
 		if (*line)
 			add_history(line);
 		write_history(history_file);
-		parsed_text = ft_split(line, ' ');
-		printf("\033[31m@maxishell: command not found: %s\033[0m\n", parsed_text[0]);
-		handle_args(&tokens, parsed_text);
-		arr = list_to_array(tokens);
-		print_stack(&tokens);
-		free_stack(&tokens);
-		free(line);
-		free(arr);
+		parsed_text = parse_input(line);
+		if (parsed_text != NULL)
+    {
+      printf("\033[31m@maxishell: command not found: %s\033[0m\n", parsed_text[0]);
+      fflush(stdout);
+  		handle_args(&tokens, parsed_text);
+  		arr = list_to_array(tokens);
+  		print_stack(&tokens);
+  		free_stack(&tokens);
+  		free(line);
+  		free(arr);
+    }
 	}
 	return (0);
 }
