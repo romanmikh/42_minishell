9c6691cfad17aa033b4242e6b12d51d74c172b62
Dmitry Demirkylych <dmdemirk@c2r7s3.42london.com>
2024-06-04 18:08:30 +0100

    add: t_minishell_data

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,28 +6,30 @@
 /*   By: dmdemirk <dmdemirk@student.42london.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/03 16:31:33 by dmdemirk          #+#    #+#             */
-/*   Updated: 2024/06/03 16:38:36 by dmdemirk         ###   ########.fr       */
+/*   Updated: 2024/06/04 12:25:47 by dmdemirk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include "shell.h"
 
-int	builtin_echo(const char **argv)
+int	builtin_echo(t_minishell_data *data)
 {
 	int	newline;
 	int	i;
 
 	newline = 1;
-	if (ft_strcmp(argv[1], "-n") == 0)
+	if (ft_strcmp(data->args[1], "-n") == 0)
 	{
 		newline = 0;
-		argv++;
+		data->args++;
 	}
-	i = -1;
-	while (*argv++)
+	i = 0;
+	while (data->args[++i])
 	{
-		write(1, argv[i], ft_strlen(argv[i]));
-		i++;
+		write(1, data->args[i], ft_strlen(data->args[i]));
+		if (data->args[i + 1])
+			write(1, " ", 1);
 	}
 	if (newline)
 		write(1, "\n", 1);
--- src/main.c
+++ src/main.c
@@ -6,12 +6,15 @@
 /*   By: dmdemirk <dmdemirk@student.42london.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/30 13:05:18 by dmdemirk          #+#    #+#             */
-/*   Updated: 2024/05/31 21:09:12 by rmikhayl         ###   ########.fr       */
+/*   Updated: 2024/06/04 12:44:38 by dmdemirk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "tokens.h"
 #include "execute.h"
+#include "shell.h"
+
+void	init_minishell_data(t_minishell_data *data, char **envp);
 
 void	handle_args(t_token **tokens, char **argv)
 {
@@ -26,14 +29,21 @@ void	handle_args(t_token **tokens, char **argv)
 }
 
 // getpwuid(geteuid())->pw_name // username, but uses disallowed functions
-int	main(void)
+int	main(int argc, char **argv, char **envp)
 {
 	char		*line;
 	int			repeat;
-	char		**arr;
 	char		**parsed_text;
 	const char	*history_file;
 	t_token		*tokens;
+	t_minishell_data	data;
+
+	init_minishell_data(&data, envp);
+	if (argc > 1)
+	{
+		printf("Usage: %s\n", argv[0]);
+		return (1);
+	}
 
 	history_file = "./utils/.maxishell_history";
 	read_history(history_file);
@@ -50,12 +60,21 @@ int	main(void)
 		parsed_text = ft_split(line, ' ');
 		// printf("\033[31m@maxishell: command not found: %s\033[0m\n", parsed_text[0]);
 		handle_args(&tokens, parsed_text);
-		arr = list_to_array(tokens);
-		execute(arr);
+		data.args = list_to_array(tokens);
+		execute(&data);
 		// print_stack(&tokens);
 		free_stack(&tokens);
 		free(line);
-		free(arr);
+		free(data.args);
 	}
 	return (0);
+}
+
+
+void	init_minishell_data(t_minishell_data *data, char **envp)
+{
+	data->args = NULL;
+	data->envp = envp;
+	data->current_dir = getcwd(NULL, 0);
+	data->exit_status = 0;
 }
