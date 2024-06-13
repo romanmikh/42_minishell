d78912ef361953dd42bbe87815c40150cbb35a67
Dima Dem <iamdimadem@gmail.com>
2024-06-13 15:56:06 +0100

    fix:cd

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -19,27 +19,21 @@
 	Functionality:
 		- Change the current directory
 		- Update the PWD and OLDPWD environment variables
-		- navigate to the home directory if no arguments are provided
-		- navigate to the previous directory if ".." is provided
+		- If no argument is provided, change to the HOME directory
+		- If the target directory does not exist, print an error message
 */
 
 int	builtin_cd(t_minishell_data *data)
 {
 	char	*target_dir;
 	char	*home_dir;
+	char	cwd[4096];
 
 	printf("builtin_cd\n");
 	home_dir = get_env(data->envp, "HOME");
 	target_dir = (char *)data->args[1];
 	if (!target_dir)
 		target_dir = home_dir;
-	else if (target_dir[0] == '~')
-		target_dir = ft_strjoin(home_dir, target_dir + 1);
-	else if (ft_strncmp(target_dir, "..", 2) == 0)
-	{
-		target_dir = get_env(data->envp, "PWD");
-		target_dir = ft_strcdup(target_dir, '/');
-	}
 	if (chdir(target_dir) != 0)
 	{
 		ft_putstr_fd("bash: cd: ", 2);
@@ -47,6 +41,7 @@ int	builtin_cd(t_minishell_data *data)
 		return (1);
 	}
 	set_env(&data->envp, "OLDPWD", get_env(data->envp, "PWD"));
-	set_env(&data->envp, "PWD", target_dir);
+	if(getcwd(cwd, sizeof(cwd)) != NULL)
+		set_env(&data->envp, "PWD", cwd);
 	return (0);
 }
