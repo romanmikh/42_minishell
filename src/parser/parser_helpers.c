33d2106b722ac48e69a9a871b4d68dbfaf80b5f3
¨Roman <roman.mikhaylenko14@hotmail.com>
2024-08-27 17:05:18 +0100

    fix: echo "rmikhayl"

--- src/parser/parser_helpers.c
+++ src/parser/parser_helpers.c
@@ -81,7 +81,11 @@ char *expand_env_var(char *arg, t_ms_data *data) {
     if (strcmp(arg, "$?") == 0) {
         return ft_itoa(data->exit_status);
     } else if (arg[0] == '$') {
-        char *env_value = getenv(arg + 1);
+		if (arg[strlen(arg) - 1] == '"') 
+		{
+            arg[strlen(arg) - 1] = '\0';
+        }
+        char *env_value = get_env(data->envp, arg + 1);
         if (env_value) {
             return ft_strdup(env_value);
         }
@@ -91,7 +95,6 @@ char *expand_env_var(char *arg, t_ms_data *data) {
 
 void post_process_command_args(t_ast *command_node, int arg_count, t_ms_data *data) {
     size_t len;
-	char *result = NULL;
     
     for (int i = 0; i < arg_count; i++) {
         char *arg = command_node->args[i];
@@ -99,14 +102,10 @@ void post_process_command_args(t_ast *command_node, int arg_count, t_ms_data *da
         if (arg[0] == '$' || (arg[0] == '"' && arg[1] == '$')) {
             if (arg[0] == '"') {
                 processed_arg = expand_env_var(arg + 1, data);
-                if (processed_arg[strlen(processed_arg) - 1] == '"') {
-                    processed_arg[strlen(processed_arg) - 1] = '\0';
-                }
             } else {
                 processed_arg = expand_env_var(arg, data);
             }
-			result = get_env(data->envp, processed_arg + 1);
-            printf(GRN"Expanded argument: %s\n"RESET, result);
+            printf(GRN"Expanded argument: %s\n"RESET, processed_arg);
         } else if (arg[0] == '\'') {
             processed_arg = ft_strdup(arg);
             printf(GRN"Unmodified argument: %s\n"RESET, processed_arg);
@@ -118,7 +117,8 @@ void post_process_command_args(t_ast *command_node, int arg_count, t_ms_data *da
     }
     for (int i = 0; i < arg_count; i++) {
         char *arg = command_node->args[i];
-        len = strlen(arg);
+		printf("current arg -> %s\n", arg);
+        len = ft_strlen(arg);
         if ((arg[0] == '"' && arg[len - 1] == '"') || (arg[0] == '\'' && arg[len - 1] == '\'')) {
             char *trimmed_arg = ft_strndup(arg + 1, len - 2);
             free(command_node->args[i]);
