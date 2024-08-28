4123ab0f024245dbf568c8140c6070e88b62bb7a
Roman Mikhaylenko <roman.mikhaylenko14@hotmail.com>
2024-08-28 19:35:37 +0100

    fix: removed /home/rocky/github/minishell from prompt

--- src/utils/prompt.c
+++ src/utils/prompt.c
@@ -6,23 +6,12 @@
 /*   By: rmikhayl <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 13:58:23 by rmikhayl          #+#    #+#             */
-/*   Updated: 2024/08/28 15:56:15 by rocky            ###   ########.fr       */
+/*   Updated: 2024/08/28 19:31:06 by rocky            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "tokens.h"
 #include "env.h"
-
-/*
-todo: 
-extract and print only after LOGNAME
-
-!wrong example:
-🌴 dmdemirk@maxishell:~/home/dmdemirk/Documents/minishell>
-
-correct example:
-🌴 dmdemirk@maxishell:~/Documents/minishell>
- */
 
 char	*generate_prompt(t_ms_data *data);
 char	*generate_prompt_string(t_ms_data *data, char *pwd, \
@@ -34,18 +23,14 @@ char	*generate_prompt(t_ms_data *data)
 	char	*prompt;
 	size_t	prompt_len;
 
-	prompt_len = strlen("🌴\e[1m @maxishell:~> \e[m") + \
-			strlen(getenv("LOGNAME")) + \
-			ft_strlen(getenv("PWD")) + \
-			1;
+	prompt_len = strlen("🌴\e[1m @maxishell$ \e[m") + \
+			strlen(getenv("LOGNAME"));
 	prompt = (char *)malloc(prompt_len);
 	if (!prompt)
 		exit(EXIT_FAILURE);
 	ft_strcpy(prompt, "🌴\e[1m ");
 	ft_strcat(prompt, getenv("LOGNAME"));
-	ft_strcat(prompt, "@maxishell:~");
-	ft_strcat(prompt, getenv("PWD"));
-	ft_strcat(prompt, "> \e[m");
+	ft_strcat(prompt, "@maxishell$ \e[m");
 	(void)data;
 	return (prompt);
 }
