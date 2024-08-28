ba4d37628cc28abd21105b2cb4ecc1d395c70987
Roman Mikhaylenko <roman.mikhaylenko14@hotmail.com>
2024-08-28 20:55:05 +0100

    fix: NORMINETTE PASSED FOR /src /inc /libgit add .git add .!

--- src/main.c
+++ src/main.c
@@ -27,10 +27,22 @@ int	status_handler(int status, t_loop_data *loop_data)
 	return (1);
 }
 
-void	main_loop(t_ms_data *data, t_loop_data *loop_data)
+void	process_ast_and_io(t_ms_data *data, t_loop_data *loop_data)
 {
 	int	status;
 
+	print_ast_root(loop_data->tree);
+	status = execute_ast(loop_data->tree, data);
+	if (status_handler(status, loop_data))
+	{
+		handle_io_fd(data);
+		loop_cleanup(loop_data->trimmed_input, loop_data->tokens, \
+				loop_data->prompt, loop_data->tree);
+	}
+}
+
+void	main_loop(t_ms_data *data, t_loop_data *loop_data)
+{
 	while (1)
 	{
 		loop_data->prompt = generate_prompt(data);
@@ -48,14 +60,7 @@ void	main_loop(t_ms_data *data, t_loop_data *loop_data)
 			continue ;
 		loop_data->tokens = tokenise(loop_data->trimmed_input);
 		loop_data->tree = parse_tokens(&loop_data->tokens, data);
-		print_ast_root(loop_data->tree);
-		status = execute_ast(loop_data->tree, data);
-		if (status_handler(status, loop_data))
-		{
-			handle_io_fd(data);
-			loop_cleanup(loop_data->trimmed_input, loop_data->tokens, \
-					loop_data->prompt, loop_data->tree);
-		}
+		process_ast_and_io(data, loop_data);
 	}
 	free(loop_data->prompt);
 }
