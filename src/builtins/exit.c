03e665867c7077cba3213d4cb2ad69d1a708d4bb
Roman Mikhaylenko <roman.mikhaylenko14@hotmail.com>
2024-11-06 14:00:30 +0000

    both error cases of exit work without leaks. Only additional exit line printed on return to fix

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -26,46 +26,50 @@ Functionality:
 - Exit with the exit status
  */
 
-void	clear_history_file(void)
-{
-	remove(HISTORY_PATH);
-}
-
-void	handle_numeric_error(t_ms_data *data, const char *arg)
-{
-	char	*temp_str;
-
-	temp_str = ft_strjoin("exit: ", arg);
-	exit_status_handler(data, NUMERIC_REQUIRED, temp_str);
-	exit(NUMERIC_REQUIRED);
-}
-
-void	handle_too_many_args_error(t_ms_data *data)
-{
-	exit_status_handler(data, TOO_MANY_ARGS, "exit");
-	exit(TOO_MANY_ARGS);
-}
-
 void free_signal_context()
 {
     t_signal_context *context = get_context(NULL);
     free(context);
 }
 
+void	clear_history_file(void)
+{
+	remove(HISTORY_PATH);
+}
+
+void cleanup_exit_resources(t_ms_data *data)
+{
+    if (data->args)
+	{
+        ft_free_2d_arr(data->args);
+        data->args = NULL;
+    }
+    clear_history_file();
+    free_ms_data(data);
+    free_signal_context(NULL);
+}
+
+void	handle_numeric_error(t_ms_data *data, const char *arg)
+{
+    char *temp_str = ft_strjoin("exit: ", arg);
+
+    exit_status_handler(data, NUMERIC_REQUIRED, temp_str);
+    free(temp_str);
+    handle_exit(data, 0);
+}
+
+void	handle_too_many_args_error(t_ms_data *data)
+{
+    exit_status_handler(data, TOO_MANY_ARGS, "exit");
+    handle_exit(data, 0);
+}
+
 void	handle_exit(t_ms_data *data, int status)
 {
-	char	*exit_status_str;
-
-	ft_putendl_fd("exit", STDOUT_FILENO);
-	data->exit_status = status;
-	exit_status_str = ft_itoa(data->exit_status);
-	set_shell_var(&data->shell_variables, "?", exit_status_str);
-	free(exit_status_str);
-	clear_history_file();
-	free_ms_data(data);
-	printf("we're about to free signal context!\n");
-	free_signal_context(NULL);
-	exit(status);
+    if (status != NUMERIC_REQUIRED && status != TOO_MANY_ARGS)
+        ft_putendl_fd("exit", STDOUT_FILENO);
+    cleanup_exit_resources(data);
+    exit(status);
 }
 
 int	builtin_exit(t_ms_data *data)
@@ -81,39 +85,16 @@ int	builtin_exit(t_ms_data *data)
 		else
 		{
 			number = ft_atoi(data->args[1]);
-			ft_free_2d_arr(data->args); // Free args before exiting
+			ft_free_2d_arr(data->args);
 			data->args = NULL;
 			handle_exit(data, number);
 		}
 	}
 	else
 	{
-		ft_free_2d_arr(data->args); // Free args before exiting
+		ft_free_2d_arr(data->args);
 		data->args = NULL;
 		handle_exit(data, 0);
 	}
-	return (0); // This line is redundant but kept for function signature consistency.
+	return (0);
 }
-
-// int	builtin_exit(t_ms_data *data)
-// {
-// 	int	number;
-
-// 	number = 0;
-// 	if (data->args[1])
-// 	{
-// 		if (ft_isnumber(data->args[1]) == 0)
-// 			handle_numeric_error(data, data->args[1]);
-// 		else if (data->args[2])
-// 			handle_too_many_args_error(data);
-// 		else
-// 		{
-// 			printf("we're in builtin_exit!\n");
-// 			number = ft_atoi(data->args[1]);
-// 			handle_exit(data, number);
-// 		}
-// 	}
-// 	else
-// 		handle_exit(data, 0);
-// 	return (0);
-// }
