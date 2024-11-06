c36cd7bfd0c6f37d16cb0361d25f0a218a685ea0
Roman Mikhaylenko <roman.mikhaylenko14@hotmail.com>
2024-11-06 10:16:21 +0000

    cat ctrl+c works, but enter must be pressed after. come back to this, solution is likely to add get_context into sigint handler

--- src/signals/signals_utils.c
+++ src/signals/signals_utils.c
@@ -16,39 +16,12 @@
 #include <unistd.h>
 #include "libft.h"
 
-// void	handle_sigint_heredoc(int signo)
-// {
-// 	(void)signo;
-// 	g_heredoc_interrupted = 1;
-// 	write(1, "\n", 1);
-// 	handle_exit
-// 	rl_replace_line("", 0);
-// 	rl_redisplay();
-
-// }
-
-
-// #include "signals.h"
-// #include <readline/readline.h>
-// #include <readline/history.h>
-// #include <unistd.h>
-// #include "libft.h"
-#include "exit_status.h"
-
 void handle_sigint_heredoc(int signo)
 {
     (void)signo;
-    
-    // Retrieve context to access data for handle_exit
-    t_signal_context *context = get_context(NULL);
-    
-    if (context && context->data_cxt)
-    {
-        handle_exit(context->data_cxt, 0);
-    }
-
     g_heredoc_interrupted = 1;
-    write(1, "\n", 1);
-    rl_replace_line("", 0);
-    rl_redisplay();
+    // write(1, "\n", 1);
+    // rl_replace_line("", 0);
+    // rl_on_new_line();
+    // rl_redisplay();
 }
